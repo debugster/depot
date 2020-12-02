f1142154851db77ece720808760ae68b8edf7ee5
Nayeem Mahmood <alive.dew@gmail.com>
2020-12-02 10:48:19 +0600

    Test created for Product model

--- test/models/product_test.rb
+++ test/models/product_test.rb
@@ -1,7 +1,43 @@
 require 'test_helper'
 
 class ProductTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "products attributes must not be empty" do
+    product = Product.new(title: "iPhone 11 Pro Max",
+                          description: "xyz",
+                          image_url: "abc.jpg")
+    product.price = -1
+    assert product.invalid?
+    assert_equal ["must be greater than or equal to 0.01"],
+        product.errors[:price]
+
+    product.price = 0
+    assert product.invalid?
+    assert_equal ["must be greater than or equal to 0.01"],
+                 product.errors[:price]
+
+    product.price = 1
+    assert product.valid?
+  end
+
+  def new_product(image_url)
+    Product.new(title: "iPhone 11 Pro Max",
+                description: "xyz",
+                price: 1,
+                image_url: image_url)
+  end
+
+  test "image url" do
+    good = %w{ fred.gif fred.jpg fred.png FRED.JPG FRED.Jpg http://a.b.c/x/y/z/fred.gif }
+    bad = %w{ fred.doc fred.gif/more fred.gif.more fred.png.pdf }
+
+    good.each do |image_url|
+      assert new_product(image_url).valid?,
+             "#{image_url} shouldn't be invalid"
+    end
+
+    bad.each do |image_url|
+      assert new_product(image_url).invalid?,
+             "#{image_url} shouldn't be valid"
+    end
+  end
 end
