6b43bdbd785bc0cd5cebb546e73ca5b098270b00
Nayeem Mahmood <alive.dew@gmail.com>
2020-12-07 15:19:21 +0600

    translation added but not rendering

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -10,7 +10,7 @@ class ApplicationController < ActionController::Base
 
   def set_i18n_locale_from_params
     if params[:locale]
-      if I18n.available_locales.map(& :to_s).include?(params[:locale])
+      if I18n.available_locales.map(&:to_s).include?(params[:locale])
         I18n.locale = params[:locale]
       else
         flash.now[:notice] = "#{params[:locale]} translations is not available now!"
